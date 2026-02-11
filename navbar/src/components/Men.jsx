85f8b1f09ad73cd4a650ddff615aa433de65e74e
Soham Ghadai <sohamghadai28@gmail.com>
2026-02-11 12:24:47 +0530

    Delete navbar directory

--- navbar/src/components/Men.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../index.css";
-
-const Men = () =>{
-    return <div className="men bg-blue-500 p-7 w-xs text-center mt-3 rounded-xl">
-        <h1 className="text-xl text-white">Male</h1>
-    </div>
-}
-export default Men;
--- navbar/src/components/Women.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../index.css";
-
-const Women = () =>{
-    return <div className="women bg-pink-500 p-7 w-xs text-center mt-3 rounded-xl">
-        <h1 className="text-xl text-white">female</h1>
-    </div>
-}
-export default Women;
